bdda58d7b7fe7b383afccde88059e87b3bdd02a7
Zian Elijah Smith <121137084+ZianElijahSmith@users.noreply.github.com>
2023-04-01 21:00:34 -0400

    Update FreeScrape.py

--- FreeScrape.py
+++ FreeScrape.py
@@ -1,52 +1,128 @@
 '''
-CopyRight - Zian Elijah Smith
+FreeScrape.py
+
+Copyright: Zian Elijah Smith
 2023
 
-FreeScrape.py rough draft
+Web Scrapper for FreePI
 '''
 
+# Pydantic is MIT license
+# Used for providing documentation and annotations to variables, etc
+try:
+    import pydantic
+except(ImportError):
+    raise ImportError("You need to install the python package pydantic")
 
-import requests
-from bs4 import BeautifulSoup
+# requests is Apache-2.0 license
+# used for making HTTP requests for webscraping
+try:
+    import requests
+except(ImportError):
+   raise ImportError("You need to install the python package requests")
+   
+# bs4 is MIT License (MIT)
+# used for scraping the HTTP data we receive from requests
+try:
+    from bs4 import BeautifulSoup
+except(ImportError):
+   raise ImportError("You need to install the python package bs4 (BeautifulSoup)")
+   
 
-# these need to be converted to dictionaries
-# but all we really NEED is a list
-free_list = []
 
-# Specific license lists
-# Will add more later
-ApacheSoftware_list = []
-GPLv2_list = []
-MIT_list = []
+# Licenses, in alphabetical order
 
-
+#1
 # Apache variable url
 # The {} at the end will be replaced with whatever page you want
-Apache_url = "https://pypi.org/search/?c=License+%3A%3A+OSI+Approved+%3A%3A+Apache+Software+License&c=Programming+Language+%3A%3A+Python&o=&q=&page={}"
+Apache_Software_License_URL = ASL =  "https://pypi.org/search/?c=License+%3A%3A+OSI+Approved+%3A%3A+Apache+Software+License&c=Programming+Language+%3A%3A+Python&o=&q=&page={}"
 
+#2
+# Artistic License
+Artistic_License_URL = AL = "https://pypi.org/search/?c=License+%3A%3A+OSI+Approved+%3A%3A+Artistic+License&o=&q=&page={}"
+
+#3
 # GPLv2 variable url
 # The {} at the end will be replaced with whatever page you want
-GPLv2_url = "https://pypi.org/search/?c=License+%3A%3A+OSI+Approved+%3A%3A+GNU+General+Public+License+v2+or+later+%28GPLv2%2B%29&o=&q=&page={}"
+GNU_Affero_General_Public_License_v3 = AGPLv3 = "https://pypi.org/search/?c=License+%3A%3A+OSI+Approved+%3A%3A+GNU+Affero+General+Public+License+v3&o=&q=&page={}"
+
+#4
+GNU_Affero_General_Public_License_v3_or_later = AGPLv3plus = "https://pypi.org/search/?c=License+%3A%3A+OSI+Approved+%3A%3A+GNU+Affero+General+Public+License+v3+or+later+%28AGPLv3%2B%29&o=&q=&page={}"
+
+#5
+GNU_Free_Documentation_License = FDL = GFDL = "https://pypi.org/search/?q=&o=&c=License+%3A%3A+OSI+Approved+%3A%3A+GNU+Free+Documentation+License+%28FDL%29&o=&q=&page={}"
+
+#6
+GNU_General_Public_License = GPL = "https://pypi.org/search/?q=&o=&c=License+%3A%3A+OSI+Approved+%3A%3A+GNU+General+Public+License+%28GPL%29&o=&q=&page={}"
+
+#7
+GNU_General_Public_License_v2 = GPLv2 = "https://pypi.org/search/?c=License+%3A%3A+OSI+Approved+%3A%3A+GNU+General+Public+License+v2+%28GPLv2%29&o=&q=&page={}"
+
+#8
+GNU_General_Public_License_v2_or_later = GPLv2plus = "https://pypi.org/search/?q=&o=&c=License+%3A%3A+OSI+Approved+%3A%3A+GNU+General+Public+License+v2+or+later+%28GPLv2%2B%29&o=&q=&page={}"
+
+#9
+GNU_General_Public_License_v3 = GPLv3 = "https://pypi.org/search/?c=License+%3A%3A+OSI+Approved+%3A%3A+GNU+General+Public+License+v3+%28GPLv3%29&o=&q=&page={}"
+
+#10
+GNU_General_Public_License_v3_or_later = GPLv3plus = "https://pypi.org/search/?c=License+%3A%3A+OSI+Approved+%3A%3A+GNU+General+Public+License+v3+or+later+%28GPLv3%2B%29&o=&q=&page={}"
+
+#11
+GNU_Lesser_General_Public_License_v2 = LGPLv2 = "https://pypi.org/search/?c=License+%3A%3A+OSI+Approved+%3A%3A+GNU+Lesser+General+Public+License+v2+%28LGPLv2%29&o=&q=&page={}"
+
+#12
+GNU_Lesser_General_Public_License_v2_or_later = LGPLv2plus = "https://pypi.org/search/?c=License+%3A%3A+OSI+Approved+%3A%3A+GNU+Lesser+General+Public+License+v2+or+later+%28LGPLv2%2B%29&o=&q=&page={}"
+
+#13
+GNU_Lesser_General_Public_License_v3 = LGPLv3 = "https://pypi.org/search/?c=License+%3A%3A+OSI+Approved+%3A%3A+GNU+Lesser+General+Public+License+v3+%28LGPLv3%29&o=&q=&page={}"
+
+#14
+GNU_Lesser_General_Public_License_v3_or_later = LGPLv3plus = "https://pypi.org/search/?c=License+%3A%3A+OSI+Approved+%3A%3A+GNU+Lesser+General+Public+License+v3+or+later+%28LGPLv3%2B%29&o=&q=&page={}"
+
+#15
+GNU_Library_or_Lesser_General_Public_License = LGPL = "https://pypi.org/search/?c=License+%3A%3A+OSI+Approved+%3A%3A+GNU+Library+or+Lesser+General+Public+License+%28LGPL%29&o=&q=&page={}"
+
+#16
+MIT_License = MIT = "https://pypi.org/search/?c=License+%3A%3A+OSI+Approved+%3A%3A+MIT+License&o=&q=&page={}"
+
+# obviously all the lists we will use will be Free_liceneses
+# However, we want to have a complete list so we know what to look for
+# Additionally, it provides the user a complete list to view
+Free_Licenses = [ Apache_Software_License_URL, Artistic_License_URL, GNU_Affero_General_Public_License_v3, GNU_Affero_General_Public_License_v3_or_later, GNU_Free_Documentation_License, GNU_General_Public_License, GNU_General_Public_License_v2, GNU_General_Public_License_v2_or_later, GNU_General_Public_License_v3, GNU_General_Public_License_v3_or_later, GNU_Lesser_General_Public_License_v2, GNU_Lesser_General_Public_License_v2_or_later, GNU_Lesser_General_Public_License_v3, GNU_Lesser_General_Public_License_v3_or_later, GNU_Library_or_Lesser_General_Public_License, MIT_License ]
 
 
-def get_packages(url):
+# GPL Compatible lists, to help users find these specifically.
+GPL_Compatible = [ Apache_Software_License_URL, Artistic_License_URL, GNU_Affero_General_Public_License_v3, GNU_Affero_General_Public_License_v3_or_later, GNU_Free_Documentation_License, GNU_General_Public_License, GNU_General_Public_License_v2, GNU_General_Public_License_v2_or_later, GNU_General_Public_License_v3, GNU_General_Public_License_v3_or_later, GNU_Lesser_General_Public_License_v2, GNU_Lesser_General_Public_License_v2_or_later, GNU_Lesser_General_Public_License_v3, GNU_Lesser_General_Public_License_v3_or_later, GNU_Library_or_Lesser_General_Public_License, MIT_License ]
+
+
+
+
+def scan_packages(url):
     dictionary = {}
     
     # We're working with page 1 atm
     # This will be edited
-    response = requests.get(url.format('1'))
+    response = requests.get( url.format('1') )
     soup = BeautifulSoup(response.text, 'html.parser')
     
     #get tags
     results = soup.select('li span')
     
-    # step count is how many steps it takes to 
+    # step count is used for iterating in steps
     step_count = 0
+    # total count takes into account EACH iteration
     total_count = 0
-    # Gives us an index on how many items there are
+    
+    # length = len(results) tells us how many items there are
     length = len(results)
+    
+    # since the items are divisible by three
+    # name, version, date
+    # this will tell us how many steps we need
     steps = length / 3
     
+    # where the magic happens
+    # .text is needed to extract the actual text FROM the html tags
     while total_count < (length-3):
         dictionary[step_count] = {'name':results[total_count].text, 'version':results[total_count+1].text, 'date':results[total_count+2].text}
         total_count += 3
@@ -54,7 +130,8 @@ def get_packages(url):
     return dictionary
 
 
-get_packages(GPLv2_url)
+# example call
+#scan_packages(GPLv2)
 
 
 
@@ -102,6 +179,8 @@ get_packages(GPLv2_url)
 
 # In order to understand the code above, you need to understand this code below
 
+
+'''
      ...: GPLv2_url = "https://pypi.org/search/?c=License+%3A%3A+OSI+Approved+%3A%3A+GNU+General+Public+License+v2+or+later+%28GPLv2%2B%29&o=&q=&page=
      ...: {}"
      ...: 
@@ -131,9 +210,12 @@ get_packages(GPLv2_url)
      ...: get_packages(GPLv2_url)
 
 
-
 '''
 
+
+
+'''
+output
      ...: get_packages(GPLv2_url)
 Out[220]: 
 {0: 'fbadmin',
