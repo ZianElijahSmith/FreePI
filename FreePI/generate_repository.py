d977582c66c1c03cd0fe27fe641a94d5e1ded458
Zian Elijah Smith <121137084+ZianElijahSmith@users.noreply.github.com>
2023-04-03 00:23:05 -0400

    Update generate_repository.py

--- FreePI/generate_repository.py
+++ FreePI/generate_repository.py
@@ -2,6 +2,7 @@
 # We need to generate something similar to https://pypi.org/simple/
 # this file will eventually import our dictionary, and then generate such a file
 
+# give it a dictionary and an empty.html file
 def generate_repository(dictionary, file):
     total_count = len(dictionary)
     count = 0
@@ -12,3 +13,17 @@ def generate_repository(dictionary, file):
                 d[count]['name'], d[count]["name"]))
             count += 1
     return file_object
+
+# Generates a file that will contain the following output
+'''
+<a href='/simple/packagedb-cli'>packagedb-cli</a>
+<a href='/simple/roibuddy'>roibuddy</a>
+<a href='/simple/pulp-cli-maven'>pulp-cli-maven</a>
+<a href='/simple/pylogstash-context'>pylogstash-context</a>
+<a href='/simple/column-print'>column-print</a>
+<a href='/simple/hopla'>hopla</a>
+<a href='/simple/uji'>uji</a>
+<a href='/simple/beaker-client'>beaker-client</a>
+<a href='/simple/q-sdk'>q-sdk</a>
+<a href='/simple/nitrate-tcms'>nitrate-tcms</a>
+'''
