d7eb99574242cdbf2a5d45367aeddc2aa1da99bc
Zian Elijah Smith <121137084+ZianElijahSmith@users.noreply.github.com>
2023-04-02 22:18:46 -0400

    Create Excluded_Packages_Dictionary.py

---
+++ FreePI/Excluded_Packages_Dictionary.py
@@ -0,0 +1,8 @@
+# Although FreePI does automated scanning, some things will have to be manually added in the approved/excluded section
+
+Excluded_Programs = {
+0: {"name":"Liberal Crime Squad", 
+    "link": "https://github.com/Kamal-Sadek/Liberal-Crime-Squad", 
+    "reason":"Although GPL licensed, this contains pre-compiled .dll files, which are proprietary and don't include source code ",
+   'excluded': "manually"}
+}
